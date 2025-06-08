b58cd7e8dd9142c026f3b59c4c88cd9675414623
tkeiei4112 <yanisa.na@dome.tu.ac.th>
2025-06-08 16:06:43 +0700

    first commit

--- lecture-hall2-booking-frontend-main/src/routes/publicRoutes.tsx
+++
@@ -1,7 +0,0 @@
-import Login from "@/pages/Login/Login";
-// import NotFound from "@/pages/common/NotFound";
-
-export const publicRoutes = [
-  { path: "/login", element: <Login />, title: "Login" },
-  // { path: "*", element: <NotFound />, title: "Not Found" },
-];
